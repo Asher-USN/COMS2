b7626cec28dab1599f200f21278ad8b1cab42b8a
asherplays <66601821+asherplays@users.noreply.github.com>
2026-03-24 12:39:07 +0200

    Added MC Lab1 with all labs

---
+++ MC/Lab1/Lab1d.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Program{
+	public static void main(String args[]){
+	Scanner in = new Scanner(System.in);
+	String s = in.nextLine();
+	char c = s.charAt(1);
+	System.out.println(c);
+	}
+}
