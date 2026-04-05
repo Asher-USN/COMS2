d4a76588743a836795c5f8cf8d32566e7579298d
asherplays <66601821+asherplays@users.noreply.github.com>
2026-04-05 18:07:56 +0200

    Added Reset on bottom of screen as well as allowing all balls to drop

--- MC/Lab4/AnimationGame/app/src/main/java/com/example/animationgame/MainActivity.java
+++ MC/Lab4/AnimationGame/app/src/main/java/com/example/animationgame/MainActivity.java
@@ -1,27 +1,42 @@
 package com.example.animationgame;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.WindowMetrics;
+
+import androidx.annotation.RequiresApi;
 
 public class MainActivity extends Activity {
 	int x=500,y=20;
-	
+	int gx=100,gy=100;
+	int yx=300,yy=50;
+	private int height =0;
+
 	DrawView drawView;
 
-	@Override
+	@SuppressLint("NewApi")
+    @Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Set full screen view
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		@SuppressLint({"NewApi", "LocalSuppress"}) WindowMetrics windowMetrics = getWindowManager().getCurrentWindowMetrics();
+
+		height = windowMetrics.getBounds().height();
+
 		drawView = new DrawView(this);
 		setContentView(drawView);
 		drawView.requestFocus();
@@ -32,10 +47,24 @@ public class MainActivity extends Activity {
 		paint.setColor(Color.RED);
 		canvas.drawCircle(x, y, 70, paint);
 		paint.setColor(Color.GREEN);
-		canvas.drawCircle(100, 100, 30, paint);
+		canvas.drawCircle(gx, gy, 30, paint);
 		paint.setColor(Color.YELLOW);
-		canvas.drawCircle(300, 50, 10, paint);
-		y=y+10;
+		canvas.drawCircle(yx, yy, 10, paint);
+		if(y>height){
+			y=0;
+		}else {
+			y = y + 10;
+		}
+		if(yy>height){
+			yy=0;
+		}else {
+			yy = yy + 15;
+		}
+		if(gy>height){
+			gy=0;
+		}else {
+			gy = gy + 20;
+		}
 	}
 
 }
