657898f0d7cf839e7fd3cf523c5c890ff15373a1
asherplays <66601821+asherplays@users.noreply.github.com>
2026-04-06 21:22:23 +0200

    Added methods into Ball class

--- MC/Lab4/AnimationGame/app/src/main/java/com/example/animationgame/Ball.java
+++ MC/Lab4/AnimationGame/app/src/main/java/com/example/animationgame/Ball.java
@@ -1,4 +1,32 @@
 package com.example.animationgame;
 
 public class Ball {
+
+    double x,y,xspeed,yspeed,max_x,max_y;
+
+    public Ball(double x, double y, double xspeed, double yspeed, double max_x, double max_y) {
+        this.x = x;
+        this.y = y;
+        this.xspeed = xspeed;
+        this.yspeed = yspeed;
+        this.max_x = max_x;
+        this.max_y = max_y;
+    }
+
+    public void update(double yAcc){
+        yspeed += yAcc;
+        y += yspeed;
+        x += xspeed;
+
+        if(y>= max_y || y<=0){
+            y*=-0.8;
+        }
+
+        if(x>= max_x || x<=0){
+            x*=-0.8;
+        }
+
+
+
+    }
 }
