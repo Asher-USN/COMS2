59d005b41879c9618cc49d3afa81b1520a981d91
asherplays <66601821+asherplays@users.noreply.github.com>
2026-04-07 10:17:47 +0200

    fixed lab 5 formatting with cntrl shift alt l and removed old code

--- MC/Lab4/AnimationGame/app/src/main/java/com/example/animationgame/Ball.java
+++ MC/Lab4/AnimationGame/app/src/main/java/com/example/animationgame/Ball.java
@@ -2,7 +2,7 @@ package com.example.animationgame;
 
 public class Ball {
 
-    double x,y,xspeed,yspeed,max_x,max_y;
+    double x, y, xspeed, yspeed, max_x, max_y;
 
     public Ball(double x, double y, double xspeed, double yspeed, double max_x, double max_y) {
         this.x = x;
@@ -13,37 +13,28 @@ public class Ball {
         this.max_y = max_y;
     }
 
-    public void update(double yAcc){
+    public void update(double yAcc) {
         yspeed += yAcc;
         y += yspeed;
         x += xspeed;
 
-       // if(y>= max_y || y<=0){
-        //    yspeed*=-0.8;
-       // }
-
-      //  if(x>= max_x || x<=0){
-       //     xspeed*=-0.8;
-      //  }
-        // Vertical Bounce
+        //vertical bounce
         if (y >= max_y) {
-            y = max_y;        // Snap back to the bottom edge
+            y = max_y;
             yspeed *= -0.8;
         } else if (y <= 0) {
-            y = 0;            // Snap back to the top edge
+            y = 0;
             yspeed *= -0.8;
         }
 
-        // Horizontal Bounce
+        //horizontal bounce
         if (x >= max_x) {
-            x = max_x;        // Snap back to the right edge
+            x = max_x;
             xspeed *= -0.8;
         } else if (x <= 0) {
-            x = 0;            // Snap back to the left edge
+            x = 0;
             xspeed *= -0.8;
         }
-
-
 
 
     }
--- MC/Lab4/AnimationGame/app/src/main/java/com/example/animationgame/MainActivity.java
+++ MC/Lab4/AnimationGame/app/src/main/java/com/example/animationgame/MainActivity.java
@@ -2,12 +2,9 @@ package com.example.animationgame;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,140 +17,74 @@ import java.util.Random;
 
 
 public class MainActivity extends Activity implements View.OnTouchListener {
-	ArrayList<Ball> balls = new ArrayList<>();
+    ArrayList<Ball> balls = new ArrayList<>();
 
-	Ball b1;
-	Ball b2;
-	Ball b3;
+    Ball b1;
+    Ball b2;
+    Ball b3;
+    DrawView drawView;
+    private double height = 0;
+    private double width = 0;
 
-	int x=500,y=20;
-	int gx=100,gy=100;
-	int yx=300,yy=50;
-	private double height =0;
-	private double width =0;
-	//private boolean up1;
-	//private boolean up2;
-	//private boolean up3;
-
-//	Bitmap myImage,myImage1,myImage2;
-
-	DrawView drawView;
-
-
-	@SuppressLint("NewApi")
+    @SuppressLint("NewApi")
     @Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// Set full screen view
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set full screen view
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-		@SuppressLint({"NewApi", "LocalSuppress"}) WindowMetrics windowMetrics = getWindowManager().getCurrentWindowMetrics();
+        @SuppressLint({"NewApi", "LocalSuppress"}) WindowMetrics windowMetrics = getWindowManager().getCurrentWindowMetrics();
 
-		height = windowMetrics.getBounds().height();
-		width = windowMetrics.getBounds().width();
-		b1 = new Ball (500 , 20 , 2 , 0 , width , height) ;
-		b2 = new Ball (100 , 100 , 3 , 0 , width , height) ;
-		b3 = new Ball (300 , 50 , 1 , 0 , width , height) ;
-		balls.add(b1);
-		balls.add(b2);
-		balls.add(b3);
-		//myImage= BitmapFactory.decodeResource(getResources(), R.drawable.ball);
-		//myImage1 = BitmapFactory.decodeResource(getResources(), R.drawable.ball1);
-		//myImage2 = BitmapFactory.decodeResource(getResources(), R.drawable.ball2);
-
-		drawView = new DrawView(this);
-		setContentView(drawView);
-		drawView.requestFocus();
-
-		drawView.setOnTouchListener(this);
-	}
+        height = windowMetrics.getBounds().height();
+        width = windowMetrics.getBounds().width();
+        b1 = new Ball(500, 20, 2, 0, width, height);
+        b2 = new Ball(100, 100, 3, 0, width, height);
+        b3 = new Ball(300, 50, 1, 0, width, height);
+        balls.add(b1);
+        balls.add(b2);
+        balls.add(b3);
 
 
-	public void doDraw(Canvas canvas, Paint paint) {
-		for(Ball b :balls){
-			paint.setColor(Color.RED);
-			b.update(0.5);
-			canvas.drawCircle((int) b.x, (int) b.y, 30, paint);
-		}
-		/*
-		paint.setColor(Color.RED);
-		canvas.drawCircle((int) b1.x, (int) b1.y, 30, paint);
-		b1.update(0.5);
+        drawView = new DrawView(this);
+        setContentView(drawView);
+        drawView.requestFocus();
 
-		//canvas.drawBitmap(myImage, x, y, paint);
-		paint.setColor(Color.GREEN);
-		canvas.drawCircle((int) b2.x, (int) b2.y, 20, paint);
-		b2.update(0.5);
-		//canvas.drawBitmap(myImage1, gx, gy, paint);
-		paint.setColor(Color.YELLOW);
-		canvas.drawCircle((int) b3.x, (int) b3.y, 10, paint);
-		b3.update(0.5);
-		//canvas.drawBitmap(myImage2, yx, yy, paint);
+        drawView.setOnTouchListener(this);
+    }
 
 
-		 */
-		/*if (!up1) {
-			y = y - 10;
-			if (y - 150 <= 0) {
-				up1 = true;
-			}
-		} else {
-			y = y + 10;
-			if (height <= y +150) {
-				up1 = false;
-			}
-		}
+    public void doDraw(Canvas canvas, Paint paint) {
+        for (Ball b : balls) {
+            paint.setColor(Color.RED);
+            b.update(0.5);
+            canvas.drawCircle((int) b.x, (int) b.y, 30, paint);
+        }
 
-		if (!up2) {
-			gy = gy - 20;
-			if (gy - 130 <= 0) {
-				up2 = true;
-			}
-		} else {
-			gy = gy + 20;
-			if (height <= gy + 130) {
-				up2 = false;
-			}
-		}
+    }
 
-		if (!up3) {
-			yy = yy - 15;
-			if (yy -110 <= 0) {
-				up3 = true;
-			}
-		} else {
-			yy = yy + 15;
-			if (height <= yy + 110) {
-				up3 = false;
-			}
-		}
-	}
-	*/
-	}
-	@Override
-	public boolean onTouch(View arg0, MotionEvent arg1) {
+    @Override
+    public boolean onTouch(View arg0, MotionEvent arg1) {
 
-		float touched = arg1.getX();
-		if(touched > (width/2)){
-			b1.y = 50;
-			b2.y = 50;
-			b3.y = 50;
-			b1.xspeed *= -1;
-			b2.xspeed *= -1;
-			b3.xspeed *= -1;
-			Random random = new Random();
+        float touched = arg1.getX();
+        if (touched > (width / 2)) {
+            b1.y = 50;
+            b2.y = 50;
+            b3.y = 50;
+            b1.xspeed *= -1;
+            b2.xspeed *= -1;
+            b3.xspeed *= -1;
+            Random random = new Random();
 
-			@SuppressLint({"NewApi", "LocalSuppress"}) double randHeight = random.nextDouble(height);
-			@SuppressLint({"NewApi", "LocalSuppress"}) double randWidth = random.nextDouble(width);
-			Ball bNew = new Ball(randWidth,randHeight,0,0,width,height);
-			balls.add(bNew);
+            @SuppressLint({"NewApi", "LocalSuppress"}) double randHeight = random.nextDouble(height);
+            @SuppressLint({"NewApi", "LocalSuppress"}) double randWidth = random.nextDouble(width);
+            Ball bNew = new Ball(randWidth, randHeight, 0, 0, width, height);
+            balls.add(bNew);
 
-		}
-		if(touched<(width/2)){
-			balls.clear();
-		}
-		return false;
-	}
+        }
+        if (touched < (width / 2)) {
+            balls.clear();
+        }
+        return false;
+    }
 }
